9ee20c379180c7729f1cbfd6df9402c4606128f1
Mu-jun <suo24691@gmail.com>
2022-08-24 11:08:29 +0900

    bbs update 구현 완료

---
+++ routes/update.js
@@ -0,0 +1,42 @@
+let express = require('express');
+const { now } = require('mongoose');
+
+let bbs = require('../models').bbs;
+let router = express.Router();
+
+router
+    .get('/:id', (req, res, next) => {
+        console.log("get update")
+        const id = req.params.id;
+        bbs.findOne({
+            where: {
+                bbs_id: id
+            }
+        }).then((result) => {
+            res.render('pages/update', {
+                bbs_id: result.bbs_id,
+                title: result.title,
+                content: result.content,
+            })
+        })
+    })
+    .post('/:id', (req, res, next) => {
+        const id = req.params.id
+        console.log('post update',id)
+
+        const body = req.body;
+        bbs.update({
+            title:body.title,
+            content:body.content,
+            u_date:now(),
+        },{
+            where:{bbs_id:id}
+        })
+        .then(result=>res.redirect('/'))
+        .catch(err => {
+            console.log(err);
+            next(err);
+        })
+    })
+
+module.exports = router;
