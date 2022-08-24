cc35cc736c0c8f2636bfb38fd4a2099424989f54
Mu-jun <suo24691@gmail.com>
2022-08-24 10:43:02 +0900

    create bbs

---
+++ routes/create.js
@@ -0,0 +1,34 @@
+let express = require('express');
+const { now } = require('mongoose');
+
+let bbs = require('../models').bbs;
+let router = express.Router();
+
+router
+    .get('/', (req, res)=>res.render('pages/write'))
+    .post('/', (req,res, next)=>{
+        console.log("create post")
+        // const body = req.body;
+        // console.log(body)
+
+        const title = req.body.title;
+        const content = req.body.content;
+        // console.log("title",title,"content",content)
+        bbs.create({
+            title:title,
+            content:content,
+            author:"admin",
+            c_date:now(),
+            u_date:now()
+        },{
+            fields: ['title','content','author','c_date','u_date']
+        })
+        .then(result=>res.redirect('/'))
+        .catch((err) => {
+            console.error(err);
+            next(err);
+        });
+        
+    })
+
+module.exports = router;
